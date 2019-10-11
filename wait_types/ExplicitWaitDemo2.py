0baaa0e3b3df93baa21e9086c76f78acba27c37f
sharif <sharifmostafa1122@gmail.com>
2019-10-11 04:58:00 +0300

    add from wait_types to correct path

--- wait_types/ExplicitWaitDemo2.py
+++ wait_types/ExplicitWaitDemo2.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import ExplicitWaitType
+from wait_types import ExplicitWaitType
 import time
 
 class ExplicitWaitDemo2():
