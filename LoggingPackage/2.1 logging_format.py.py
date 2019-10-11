4929b4b50e9240b7d700b24eef30af12bcb39363
sharif <sharifmostafa1122@gmail.com>
2019-10-11 11:30:48 +0300

    play with logging format with 2 links for more editions options

---
+++ LoggingPackage/2.1 logging_format.py.py
@@ -0,0 +1,13 @@
+"""
+Logging Format
+https://docs.python.org/3/library/logging.html#logrecord-attributes
+https://docs.python.org/3/library/time.html#time.strftime
+"""
+import logging
+
+logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.DEBUG)
+logging.warning("warning message")
+logging.info("info message")
+logging.error("error message")
+# %I for 12 hours and %H for 24 base   time
